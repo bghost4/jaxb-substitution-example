8b53d9fd99e3569ec15c08f0ffa16c1f1f1eaafe
Delbert Martin IV <dmartin@rheonix.com>
2021-10-01 09:59:08 -0400

    attempting to find correct fixes for plugin

--- src/main/java/com/derpderphurr/Example.java
+++ src/main/java/com/derpderphurr/Example.java
@@ -10,13 +10,28 @@ import java.io.InputStream;
 
 public class Example {
     public static void main(String[] args) {
-        InputStream is = Example.class.getClassLoader().getResourceAsStream("/MyGarage.xml");
+        InputStream is = Example.class.getClassLoader().getResourceAsStream("MyGarage.xml");
         StreamSource ss = new StreamSource(is);
         JAXBContext ctx = null;
+        if(is == null || ss == null) {
+            System.exit(1);
+        }
+        System.out.println("Found XML File");
         try {
             ctx = JAXBContext.newInstance("com.example.sub");
-            JAXBElement<GarageType> garage =  ctx.createUnmarshaller().unmarshal(ss, GarageType.class);
-            garage.getValue().getvehicle().stream().forEach(v -> System.out.printf("Vechicle %s\n",v.getModel()));
+            //ctx = JAXBContext.newInstance(com.example.sub.ObjectFactory.class);
+            JAXBElement<GarageType> garage = ctx.createUnmarshaller().unmarshal(ss, GarageType.class);
+
+            System.out.println("Garage Owner: "+
+            garage.getValue().getOwnerName()
+            );
+
+            if (garage.getValue().getVehicle().size() > 0){
+                garage.getValue().getVehicle().stream().forEach(v -> System.out.printf("Vechicle %s\n", v.getModel()));
+            } else {
+                System.out.println("No Vehicles Found");
+            }
+
         } catch (JAXBException e) {
             e.printStackTrace();
         }
